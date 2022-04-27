637183139f3304981dc369eba521ea84582bbbcf
wabi\burkiince <burkiince35@gmail.com>
2022-04-27 18:49:39 +0300

    lazer ekledim

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    Rigidbody physic;
+    [SerializeField] float speed;
+    void Start()
+    {
+        physic = GetComponent<Rigidbody>();
+        physic.velocity = transform.forward*speed;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
