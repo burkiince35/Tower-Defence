932cb8826b775a924bbe6d873b486a86a9322416
wabi\burkiince <burkiince35@gmail.com>
2022-04-29 16:00:01 +0300

    enemylere hareket verildi

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed = 5f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized*speed*Time.deltaTime,Space.World);
+        if (Vector3.Distance(transform.position, target.position)<=0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    private void GetNextWayPoint()
+    {
+        if (wavepointIndex >= waypoints.points.Length-1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = waypoints.points[wavepointIndex];
+    }
+}
