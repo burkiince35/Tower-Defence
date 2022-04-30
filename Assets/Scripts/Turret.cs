469428b03a5161e67945d861b19ede6001eea0eb
wabi\burkiince <burkiince35@gmail.com>
2022-04-30 18:10:05 +0300

    bullet ve scripti eklendi

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,19 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+    [Header("Attributes")]
     public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    [Header("Unity Setup Fields")]
     public string enemytag = "enemy";
+
     public Transform PartToRotate;
     public float TurnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
     void Start()
     {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -49,6 +59,23 @@ public class Turret : MonoBehaviour
         Quaternion lookdirection = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation,lookdirection,Time.deltaTime*TurnSpeed).eulerAngles;
         PartToRotate.rotation = Quaternion.Euler(0f,rotation.y,0f);
+
+        if (fireCountdown <=0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletgo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet bllt = bulletgo.GetComponent<bullet>();
+        if (bllt)
+        {
+            bllt.seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected()
