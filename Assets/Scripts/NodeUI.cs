c368e1e53e68935f04685c47412081a0a82067ab
wabi\burkiince <burkiince35@gmail.com>
2022-05-19 12:01:17 +0300

    son commit

--- Assets/Scripts/NodeUI.cs
+++ Assets/Scripts/NodeUI.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class NodeUI : MonoBehaviour
 {
     public GameObject ui;
+    public Button UpgradeButton;
+    public TextMeshProUGUI upgradeCost;
     private Node target;
     public void SetTarget(Node _target)
     {
         target = _target;
         transform.position = target.GetBuildPosition();
+        if (!target.isUpgraded)
+        {
+            upgradeCost.text = "$" + target.turretblueprint.Upgradecost;
+            UpgradeButton.interactable = true;
+        }
+        else
+        {
+            upgradeCost.text = "Done";
+            UpgradeButton.interactable = false;
+        }
+        
         ui.SetActive(true);
     }
 
